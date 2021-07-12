62fbd4a600bc369764475763f7e61ce2b61a18ff
Nikola Barbarić <nikolainfinum@gmail.com>
2021-07-12 14:20:38 +0200

    change text to "third app"

--- TV Shows/TV Shows/ViewController.swift
+++ TV Shows/TV Shows/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.titleLabel.text = "This is my third app"
     }
 
 
