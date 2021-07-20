f8d78d5ce1a309c5ea6f842039ad7dd3495a7eff
Nikola Barbarić <nikolainfinum@gmail.com>
2021-07-20 17:36:55 +0200

    added all elements and constraints

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -15,13 +15,11 @@ class LoginViewController : UIViewController{
     
     //MARK: - Outlets
     
-    @IBOutlet private weak var changeStateButton: UIButton!
-    @IBOutlet private weak var textLabel: UILabel!
-    @IBOutlet private weak var indicatorView: UIActivityIndicatorView!
+    
     
     //MARK: - Properties
     
-    private var numberOfTaps: Int = 0
+    
     
     //MARK: - Lifecycle methods
     
@@ -32,35 +30,14 @@ class LoginViewController : UIViewController{
     
     //MARK: - Actions
     
-    @IBAction func changeStateButtonActionHandler() {
-        print("button tapped")
-        numberOfTaps += 1
-        textLabel.text = "Tapped \(numberOfTaps) times"
-        
-        if indicatorView.isAnimating{
-            indicatorView.stopAnimating()
-            changeStateButton.setTitle("Start", for: .normal)
-        } else {
-            indicatorView.startAnimating()
-            changeStateButton.setTitle("Stop", for: .normal)
-        }
-    }
+   
+    
     
     //MARK: - Private functions
     
     private func configureUI(){
-        changeStateButton.layer.cornerRadius = 25
-        changeStateButton.clipsToBounds = true
-        indicatorView.hidesWhenStopped = true
         
-        showIndicatorView(forSeconds: 3)
-    }
-    
-    private func showIndicatorView(forSeconds: Int){
-        indicatorView.startAnimating()
-        delay(for: 3.0){
-            self.indicatorView.stopAnimating()
-        }
+        
     }
     
     private func delay(for delay: Double, handler: @escaping () -> Void) {
