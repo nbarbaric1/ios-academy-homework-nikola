ebe21866273367df4015a67a24b701c193fe0940
Nikola Barbarić <nikolainfinum@gmail.com>
2021-07-20 15:13:31 +0200

    issues solved

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -5,54 +5,67 @@
 //  Created by Infinum on 12.07.2021..
 //
 
+// MARK: Imports
+
 import Foundation
 import UIKit
 import SVProgressHUD
 
 class LoginViewController : UIViewController{
     
-    @IBOutlet weak var tapMeButton: UIButton!
-    @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
-    var numOfTaps = 0
-    var indicatorOn = false
+    //MARK: - Outlets
+    
+    @IBOutlet private weak var changeStateButton: UIButton!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var indicatorView: UIActivityIndicatorView!
+    
+    //MARK: - Properties
+    
+    private var numberOfTaps: Int = 0
+    
+    //MARK: - Lifecycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tapMeButton.layer.cornerRadius = 25
-        tapMeButton.clipsToBounds = true
-        
-        indicatorView.hidesWhenStopped = true
-//        indicatorView.startAnimating()
-//        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
-//            self.indicatorView.stopAnimating()
-//        }
-        
-        SVProgressHUD.show()
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
-            SVProgressHUD.dismiss()
-                }
-        
+        configureUI()
     }
     
-    @IBAction func buttonActionHandler(_ sender: Any) {
+    //MARK: - Actions
+    
+    @IBAction func changeStateButtonActionHandler() {
         print("button tapped")
-        numOfTaps += 1
-        textLabel.text = "Tapped \(numOfTaps) times"
+        numberOfTaps += 1
+        textLabel.text = "Tapped \(numberOfTaps) times"
         
-        if indicatorOn == false{
-            indicatorView.startAnimating()
-            indicatorOn = true
-        }
-        else {
+        if indicatorView.isAnimating{
             indicatorView.stopAnimating()
-            indicatorOn = false
+            changeStateButton.setTitle("Start", for: .normal)
+        } else {
+            indicatorView.startAnimating()
+            changeStateButton.setTitle("Stop", for: .normal)
         }
     }
     
+    //MARK: - Private functions
     
+    private func configureUI(){
+        changeStateButton.layer.cornerRadius = 25
+        changeStateButton.clipsToBounds = true
+        indicatorView.hidesWhenStopped = true
+        
+        showIndicatorView(forSeconds: 3)
+    }
     
+    private func showIndicatorView(forSeconds: Int){
+        indicatorView.startAnimating()
+        delay(for: 3.0){
+            self.indicatorView.stopAnimating()
+        }
+    }
     
-    
+    private func delay(for delay: Double, handler: @escaping () -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay){
+            handler()
+        }
+    }
 }
