c0d46a101fc5edbc7f3ea9b334dbcc3ddc8176a1
Nikola Barbarić <nikolainfinum@gmail.com>
2021-07-24 17:03:51 +0200

    updated

---
+++ TV Shows/TV Shows/Extensions/UIViewController+KeyboardHandling.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+KeyboardHandling.swift
+//  TV Shows
+//
+//  Created by Infinum on 24.07.2021..
+//
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class LoginViewController : UIViewController{
     
-    //MARK: - Outlets
+    // MARK: - Outlets
     
     @IBOutlet private weak var emailTextfield: UITextField!
     @IBOutlet private weak var passwordTextfield: UITextField!
@@ -19,58 +19,22 @@ class LoginViewController : UIViewController{
     @IBOutlet private weak var loginButton: UIButton!
     @IBOutlet private weak var seePasswordButton: UIButton!
     
-    //MARK: - Properties
-    
-    
-    //MARK: - Lifecycle methods
+    // MARK: - Lifecycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
         configureUI()
     }
+}
+
+// MARK: - Extensions
+
+    // MARK: - Utility
+
+private extension LoginViewController{
     
-    //MARK: - Actions
-    
-    @IBAction private func rememberCheckButtonActionHandler() {
-        if rememberCheckButton.isSelected{
-            rememberCheckButton.isSelected = false
-            rememberCheckButton.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
-        } else{
-            rememberCheckButton.isSelected = true
-            rememberCheckButton.setImage(UIImage(named: "ic-checkbox-selected"), for: .normal)
-        }
-    }
-    
-    @IBAction func emailTextfieldEditingDidEndActionHandler() {
-        if let input = emailTextfield.text{
-            if isValidEmail(input) {
-                loginButton.isEnabled = true
-                loginButton.backgroundColor = .white
-                loginButton.setTitleColor(.myPurple, for: .normal)
-            } else{
-                loginButton.isEnabled = false
-                loginButton.backgroundColor = .white.withAlphaComponent(0.3)
-                loginButton.setTitleColor(.white.withAlphaComponent(0.4), for: .normal)
-            }
-        }
-    }
-    
-    @IBAction func seePasswordButtonActionHandler() {
-        if seePasswordButton.isSelected{
-            seePasswordButton.isSelected = false
-            seePasswordButton.setImage(UIImage(named: "ic-visible"), for: .normal)
-            passwordTextfield.isSecureTextEntry = true
-        } else{
-            seePasswordButton.isSelected = true
-            seePasswordButton.setImage(UIImage(named: "ic-invisible"), for: .normal)
-            passwordTextfield.isSecureTextEntry = false
-        }
-    }
-    
-    //MARK: - Private functions
-    
-    private func configureUI(){
+    func configureUI() {
         emailTextfield.attributedPlaceholder = NSAttributedString(
             string: "Email",
             attributes: [NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.7)])
@@ -80,28 +44,31 @@ class LoginViewController : UIViewController{
         loginButton.layer.cornerRadius = 21.5
         loginButton.clipsToBounds = true
     }
+}
+
+    // MARK: - IBActions
+
+private extension LoginViewController{
     
-    private func isValidEmail(_ email: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
-        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: email)
+    @IBAction func rememberCheckButtonActionHandler() {
+        rememberCheckButton.isSelected.toggle()
     }
-}
-
-    //MARK: - Extensions
-
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
+    
+    @IBAction func emailTextfieldEditingDidEndActionHandler() {
+        guard let input: String = emailTextfield.text else { return }
+        if input.isValidEmail() {
+            loginButton.isEnabled = true
+            loginButton.backgroundColor = .white
+            loginButton.setTitleColor(.myPurple, for: .normal)
+        } else {
+            loginButton.isEnabled = false
+            loginButton.backgroundColor = .white.withAlphaComponent(0.3)
+            loginButton.setTitleColor(.white.withAlphaComponent(0.4), for: .normal)
+        }
     }
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
+    
+    @IBAction func seePasswordButtonActionHandler() {
+        seePasswordButton.isSelected.toggle()
+        passwordTextfield.isSecureTextEntry.toggle()
     }
-}
-
-extension UIColor {
-  static let myPurple = #colorLiteral(red: 0.3215686275, green: 0.2117647059, blue: 0.5490196078, alpha: 1)
 }
